1bbcbb4238454aa24d0815695e82e68061e7323c
Tomi <tomi.ambro94@gmail.com>
2021-12-10 19:10:05 -0300

    Adding streamlit app and requirements

---
+++ covid_app.py
@@ -0,0 +1,38 @@
+from fastai.vision.widgets import *
+from fastai.vision.all import *
+
+from pathlib import Path
+
+import streamlit as st
+
+class Predict:
+    def __init__(self, filename):
+        self.learn_inference = load_learner(Path()/filename)
+        self.img = self.get_image_from_upload()
+        if self.img is not None:
+            self.display_output()
+            self.get_prediction()
+    
+    @staticmethod
+    def get_image_from_upload():
+        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+        if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
+
+    def display_output(self):
+        st.image(self.img.to_thumb(500,500), caption='Uploaded Image')
+
+    def get_prediction(self):
+
+        if st.button('Classify'):
+            pred, pred_idx, probs = self.learn_inference.predict(self.img)
+            st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+        else: 
+            st.write(f'Click the button to classify') 
+
+if __name__=='__main__':
+
+    file_name='dog.pkl'
+
+    predictor = Predict(file_name)
