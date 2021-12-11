9c10fd055a3384a3e96dde626573e06622d117d1
Tomi <tomi.ambro94@gmail.com>
2021-12-10 23:53:10 -0300

    Adding descriptions and images

--- covid_app.py
+++ covid_app.py
@@ -4,17 +4,10 @@ import os
 from pathlib import Path
 import streamlit as st
 
-# def load_learner(file):
-#     print(file)
 
-# class PILImage:
-#     @staticmethod
-#     def create(file):
-#         print(file)
-
-class Predict:
+class Predictor:
     def __init__(self, filename):
-        self.learn_inference = load_learner(filename)
+        self.learner = load_learner(filename)
         self.img = self.get_image_from_upload()
         if self.img is not None:
             self.display_output()
@@ -33,15 +26,24 @@ class Predict:
     def get_prediction(self):
 
         if st.button('Classify'):
-            pred, pred_idx, probs = self.learn_inference.predict(self.img)
+            pred, pred_idx, probs = self.learner.predict(self.img)
             st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
         else: 
             st.write(f'Click the button to classify') 
 
 if __name__=='__main__':
-    
+    st.write("# Covid detection from CT Scans")
+    st.write("### How precise is it?")
+    st.write("Here is the confusion matrix of the test set results:")
+    st.image('cm.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.write("Here is the F1 score from training:")
+    st.image('results.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.write("### How does it work?")
+    st.write("Upload your CT scan image below")
+    st.write("Sample image:")
+    st.image('ct_sample.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     path = Path('export.pkl')
     if path.is_file():
         file_name='export.pkl'
-        predictor = Predict(file_name)
+        predictor = Predictor(file_name)
 
